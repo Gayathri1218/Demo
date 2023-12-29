23e0ddf087ee185df2044ed0b9c21410398e1289
Gayathri1218 <124231207+Gayathri1218@users.noreply.github.com>
2023-12-29 12:13:28 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,14 @@
 import streamlit as st
 st.set_page_config(page_title='cats')
 st.markdown("## Types of cats")
-st.write("- Persian cat")
-st.image("./pcat.jpg")
-st.write("- white cat")
-st.image("./wcat.jpg", width=280)
-st.write("- cat")
-st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMtz__o1a8lG_sKQIu47iK0b1neWUBdwBNfg&usqp=CAU")
+col1,col2,col3=st.columns(3)
+with col1:
+  st.subheader("Persian cat")
+  st.image("./pcat.jpg",caption="persian cat",use_column_width=True)
+  st.write("persian cats are cute")
+with col2:
+  st.write("- white cat")
+  st.image("./wcat.jpg", width=280,use_column_width=True)
+with col3:
+  st.write("- cat")
+  st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMtz__o1a8lG_sKQIu47iK0b1neWUBdwBNfg&usqp=CAU",use_column_width=True)
