4ec0fbaf48f8aff4872a30f6554b5fcb5c57ad81
Gayathri1218 <124231207+Gayathri1218@users.noreply.github.com>
2023-12-29 11:38:15 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,5 +2,6 @@ import streamlit as st
 st.set_page_config(page_title='cats')
 st.markdown("## Types of cats")
 st.write("- Persian cat")
-st.image(image="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.azenmacskam.hu%2Fwp-content%2Fuploads%2F2022%2F02%2Fgettyimages-174776419.jpg&tbnid=ipwWl1a2vrB1DM&vet=12ahUKEwizmqrs9LODAxUCSmwGHZPZA_wQMygXegUIARCiAQ..i&imgrefurl=https%3A%2F%2Fwww.lovemycatz.com%2Fbreed-description%2Fpersian-cat-breed-description-the-dignified-calm-loving-aristocrat%2F&docid=sJfsgshiWIPN9M&w=1200&h=800&q=persian%20cat&ved=2ahUKEwizmqrs9LODAxUCSmwGHZPZA_wQMygXegUIARCiAQ")
+st.write("./pcat.jpg")
 st.write("- white cat")
+st.write("./wcat.jpg")
