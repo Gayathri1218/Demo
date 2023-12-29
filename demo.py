9c681e3d0a2e57c15fa514f46cdd99ad74397c65
Gayathri1218 <124231207+Gayathri1218@users.noreply.github.com>
2023-12-29 11:39:40 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,6 +2,6 @@ import streamlit as st
 st.set_page_config(page_title='cats')
 st.markdown("## Types of cats")
 st.write("- Persian cat")
-st.write("./pcat.jpg")
+st.image("./pcat.jpg")
 st.write("- white cat")
-st.write("./wcat.jpg")
+st.image("./wcat.jpg")
